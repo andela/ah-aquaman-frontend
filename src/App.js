33b663380763987966c80f3a811844e3f6309802
wasibani roy <roy.wasibani@andela.com>
2019-04-01 15:52:29 +0300

    chore(react): Create react app
    - create an empty react application
    
    [Finishes #164069171]

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./assets/App.css";
+
+class App extends Component {
+    render() {
+        return (
+          <div>
+              <h1>Authors Haven Application!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
